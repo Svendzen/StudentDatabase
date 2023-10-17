0e4ac295cfe8a8f75f9146f8f893dc456fd5bd35
Johan Svendsen <johan.svendsen@hotmail.com>
2023-10-17 12:13:15 +0200

    client setup

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>HELLOOÕÓŒ</h1>);
+root.render(<h1>hello !</h1>);
